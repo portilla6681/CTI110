985dbab992c9e67153a2e9b249530d1253769269
portilla6681 <portilla6681@student.faytechcc.edu>
2024-10-08 18:05:41 +0000

    p1hw2

---
+++ CTI110_Python/P1HW2_Portillo.py
@@ -0,0 +1,31 @@
+# CTI110
+# P1HW2
+# A PORTILLO
+# 10/8/2024
+
+'''
+3. Ask user to enter their budget
+4. Ask user to enter travel destination
+5. Ask user for amount they will spend on gas
+6. Ask user for amount they will spend on accommodation
+7. Ask user for amount they will spend on food
+8. Add expenses
+9. Subtract expenses from budget
+10. Display results
+'''
+print(" Trip Calculator ")
+# Ask user to enter their budget
+budget = float(input("What is your travel budget? $ "))
+# Ask user to enter travel destination
+destination = input("Where are you going?")
+# Ask user for amount they will spend on gas
+gas = input("How much will you spend on gas?")
+# Ask user for amount they will spend on accommidation
+hotel = input ("How much wil you spend on lodging?")
+# Ask user for amunt they will spend on food
+food = input ("How much will you spend on food?")
+# Add expenses
+
+# Subtrat expenses from budget
+
+# Display Results 
