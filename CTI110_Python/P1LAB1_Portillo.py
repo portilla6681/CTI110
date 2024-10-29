8f439d2dd90b705c90142f2af04e6a836af59ccd
portilla6681 <portilla6681@student.faytechcc.edu>
2024-10-29 15:08:47 -0400

    Update P1LAB1_Portillo.py

--- CTI110_Python/P1LAB1_Portillo.py
+++ CTI110_Python/P1LAB1_Portillo.py
@@ -1,1 +1,9 @@
+ # Portillo, Armando
+ # 10/29/24
+ # P1LAB1
+ 
 
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+
+print('Hello,', first_name, last_name +'! Welcome to CTI-110.')
